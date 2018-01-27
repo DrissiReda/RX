cecbf4dbcad8d697e10493bf6db03182b7563b12
drissireda <mohamedreda.drissi@edu.uca.ma>
2018-01-27 14:19:05 +0100

    Initial Commit

---
+++ init.hpp
@@ -0,0 +1,11 @@
+#define Antenne 6
+#define BBU     2
+#include <array>
+#include <algorithm>
+#include <iostream>
+#include <ctime>
+srand(getpid());
+using namespace std;
+
+int init_G(array A,int i,int j);
+float Cost;
