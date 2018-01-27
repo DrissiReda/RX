dacbf54c884332aab192d672be88c3d77a8a3d48
drissireda <mohamedreda.drissi@edu.uca.ma>
2018-01-27 14:38:21 +0100

    Folders

--- rand.cpp
+++
@@ -1,9 +0,0 @@
-#include "random.hpp"
-
-int Random::operator()(int min, int max) {
-	return std::uniform_int_distribution<int>{min, max}(engine);
-}
-
-bool Random::operator()(double p) {
-	return std::bernoulli_distribution{p}(engine);
-}
